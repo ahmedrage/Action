4379db4e9852a352f70fd2544f07f6bce51799f8
ahmedrage <ahmedrage@hotmail.com>
2015-11-02 22:15:52 +1030

    Added flipping of player character when moving left.
    -Note to self code needs cleaning

--- 2dAction/Assets/Scripts/MyCharacterController.cs
+++ 2dAction/Assets/Scripts/MyCharacterController.cs
@@ -7,12 +7,20 @@ public class MyCharacterController : MonoBehaviour {
 	public bool grounded;
 	public float Speed;
 	public float jumpForce;
+	private float move;
+	public bool facingRight;
+	public Transform playerGraphics;
 	void Start () {
+		move = Input.GetAxis ("Horizontal");
+		playerGraphics = transform.FindChild("Rob");
+		facingRight = true;
 	}
 	void Update () {
 		Move ();
 	}
 	void Move () {
+		move = Input.GetAxis ("Horizontal");
+
 		// Jumping
 		if (Rb.velocity.y == 0)
 			grounded = true;
@@ -28,10 +36,25 @@ public class MyCharacterController : MonoBehaviour {
 
 		// Movement
 
-		Vector3 pos = Player.transform.position;
-		pos.x = pos.x + Input.GetAxis ("Horizontal") /100 * Speed ;
-		Player.transform.position = pos ;
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * Speed, GetComponent<Rigidbody2D>().velocity.y);
+
+		if (move > 0 && !facingRight) {
+			Flip ();
+		} else if (move < 0 && facingRight) {
+			Flip ();
+		}
+
+	}
+
+	void Flip () {
+		facingRight = !facingRight;
+		
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = playerGraphics.localScale;
+		theScale.x *= -1;
+		playerGraphics.localScale = theScale;
 	}
 
 }
 
+//GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
