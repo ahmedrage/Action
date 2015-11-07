41b8e51a6474796b3aa089186f1afa408169bd1a
ahmedrage <ahmedrage@hotmail.com>
2015-11-08 00:13:32 +1030

    Added first build.
    Make sure to click windowed and the idle is not in there at the moment.

---
+++ 2dAction/Assets/armRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class armRotation : MonoBehaviour {
+
+	public int rotationOffset = -90;
+
+	void Update () {
+		Vector3 differince = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		differince.Normalize ();
+
+		float rotZ = Mathf.Atan2 (differince.y, differince.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ + rotationOffset);
+	}
+}
